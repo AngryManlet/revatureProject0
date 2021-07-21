d8bcadd4edb517e2b55a640177c95905a27b896b
Miguel <migcsomosot@gmail.com>
2021-07-21 17:00:03 -0500

    4th push

--- src/main/scala/project_0.scala
+++ src/main/scala/project_0.scala
@@ -4,7 +4,8 @@ import scala.io.StdIn._
 // Creating object
 object simpleBank
 {
-	//variables storing starting values
+	// variables storing starting values
+	// need to learn how to use collections to store username and password to allow for user creation
 	var userName: String = "user1"
 	var passWord: String = "password1"
 	var accountMoney: Double = 1000.00
@@ -18,7 +19,8 @@ object simpleBank
 		var netAmount = x + depositAmount
 		return netAmount
 	} 
-	//Method for withdrawing money, asking again if money to be withdrawn is greater than money in account
+	// Method for withdrawing money, asking again if money to be withdrawn is greater than money in account
+	// should add overdraft detection and allow for negative values to more closely follow real world application
 	def withdraw(x:Double) : Double =
 	{
 		print("Enter your withdrawal amount: ")
@@ -36,6 +38,8 @@ object simpleBank
 		
 	}
 	// Main method, asks for username and password, runs again if wrong username and password, and asks for actions to take, ends if asked
+	// should add timeout feature if time passes, say, 10 mins, to immediately log out for security reasons, as well as separating the feature of account status to another option
+	// should also add a use for account status, perhaps better interest fees, or waived monthly fees
 	def main(args: Array[String])
 	{
 		println("Please enter your username and password: ")
@@ -63,7 +67,7 @@ object simpleBank
 				{
 					println("Please try again.")
 				}
-			if (accountMoney > 1000000)
+			if (accountMoney > 10000)
 				{
 					accountStatus = "Gold"
 				}
